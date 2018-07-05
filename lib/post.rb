0587b0507edf5965450d854edaf14e77f6a20dd1
Priscilla H <priscillamhuynh@gmail.com>
2018-07-05 10:18:43 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -9,10 +9,10 @@ class Post
   end
 
   def author_name
-      if self.author
-        self.author.name
-      else
-        nil
-      end
+    if self.author
+      self.author.name
+    else
+      nil
+    end
   end
 end
