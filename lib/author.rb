09142df8f4980a7be02f49215c33cb55031268fe
Priscilla H <priscillamhuynh@gmail.com>
2018-07-05 10:05:32 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,11 @@
 class Author
   attr_accessor :name
-  @@song_count=0
+  @@posts=0
 
   def initialize(name)
     @name=name
     @posts=[]
-    @@song_count+=1
+    @@posts+=1
   end
 
   def posts
