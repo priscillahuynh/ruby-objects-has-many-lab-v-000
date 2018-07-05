3117a048af4a99bc6b41bacd3c9814d8af000486
Priscilla H <priscillamhuynh@gmail.com>
2018-07-05 10:07:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
   attr_accessor :name
   @@song_count=0
-  
+
   def initialize(name)
     @name=name
     @songs=[]
@@ -14,6 +14,7 @@ class Artist
   def add_song(song)
     self.songs<<song
     song.artist=self
+    @@song_count+=1
   end
 
   def add_song_by_name(song)
